a13409e1c572ff23614d6e21909a6c78fdc8912f
听寒 <inkedawn@gmail.com>
2021-02-06 16:32:54 +0800

    更换数位计数方法，以修复潜在的bug
    浮点运算的精度误差可能导致数位被错误计算（取整后与真实结果差1）。

--- 2006/7.cpp
+++ 2006/7.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 // 将一个数的个位移到首位后，如1234-4123
 int f(int x){
-    int decwidth = floor(log(x)/log(10)); //求其十进制表示下的位数
-    return (x%10)*int(pow(10,decwidth))+x/10;
+    if(x==0) return 0;
+    int decwidth = 0, t=x; //求其十进制表示下的位数
+    while(t){++decwidth; t/=10;}
+    return (x%10)*int(pow(10,decwidth-1))+x/10;
 }
 int main() {
     int i=10;
     while(!( f(i)==4*i )) ++i;
     printf("%d", i);
+    
 }
 
