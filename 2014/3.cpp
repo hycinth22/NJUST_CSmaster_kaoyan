67ef4ba9aff798bcc9cbd89ca6fa78dab81cebf3
听寒 <10476912+pastrale@users.noreply.github.com>
2021-02-10 14:34:20 +0800

    修复不正确的计算

--- 2014/3.cpp
+++ 2014/3.cpp
@@ -3,13 +3,10 @@ using namespace std;
 int main() {
     int n; scanf("%d",&n);
     int ngroup = (n%8==0?n/8:n/8+1); //使分组数最少
-    if(n%ngroup==0)
-        for(int i=0;i<ngroup;++i)
-            printf("%d\n", n/ngroup);
-    else{
-        for(int i=0;i<ngroup-1;++i)
-            printf("%d\n", n/ngroup+1);
+    int k = n%ngroup;
+    for(int i=0;i<k;++i)
+        printf("%d\n", n/ngroup+1);
+    for(int i=0;i<ngroup-k;++i)
         printf("%d\n", n/ngroup);
-    }
 }
 
